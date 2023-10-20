bb0f0ecd07c301f93dadab90ddd0d81fb9322517
Thaylor Lambrecht <91397478+ThaylorLT@users.noreply.github.com>
2023-10-20 19:48:37 +0000

    addd temp measurement

--- main.ino
+++ main.ino
@@ -15,28 +15,18 @@ void printSpeed(){
 }
 
 void spinMotor(){
-    
-    servo.write(0); // move MG996R's shaft to angle 0°
-  delay(1000); // wait for one second
-  servo.write(45); // move MG996R's shaft to angle 45°
-  delay(1000); // wait for one second 
-  servo.write(90); // move MG996R's shaft to angle 90°
-  delay(1000); // wait for one second
-  servo.write(135); // move MG996R's shaft to angle 135°
-  delay(1000); // wait for one second
-  servo.write(180); // move MG996R's shaft to angle 180°
-  delay(1000); // wait for one second
+    //falta o motor
 }
 
+int measureTemperature(){
+    
+}
 
 void setup(){
-    servo.attach(6);
     lcd.init();
     lcd.setBacklight(HIGH);
 }
 
 loop(){
-    //printSpeed();
-    spinMotor();
     delay(1000);
 }
